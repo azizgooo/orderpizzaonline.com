b2ff8bdd5ba7a56837e277d515f40c72ca123685
azizgooo <132129683+azizgooo@users.noreply.github.com>
2023-04-29 13:08:56 +0500

    Add files via upload

---
+++ fooditem.js
@@ -0,0 +1,404 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Big Chicken Pizza',
+    category : 'biryani',
+    rating : 4.3,
+    price: 13,
+    img: 'images/buffalo/bigChickenPizza.jpg',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Buffalo Chicken Pizza',
+    category : 'biryani',
+    rating : 4.3,
+    price: 15,
+    img: 'images/buffalo/buffaloPizzaWithKale.jpg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Buffalo Olives Pizza',
+    category : 'biryani',
+    rating : 4.3,
+    price: 18,
+    img: 'images/buffalo/buffaloPizzaWithOlives.jpg',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Buffalo Spicy Pizza',
+    category : 'biryani',
+    rating : 4.3,
+    price: 15,
+    img: 'images/buffalo/BuffaloSpicyPizza.jpg',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Classic Buffalo Pizza',
+    category : 'biryani',
+    rating : 4.3,
+    price: 10,
+    img: 'images/buffalo/classicBuffaloPizza.jpg',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'Americano Pizza',
+    category : 'biryani',
+    rating : 4.3,
+    price: 12,
+    img: 'images/buffalo/bigChickenPizza.jpg',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'Americano Kale Pizza',
+    category : 'biryani',
+    rating : 4.3,
+    price: 13,
+    img: 'images/buffalo/buffaloPizzaWithKale.jpg',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'Spicy Pizza',
+    category : 'biryani',
+    rating : 4.3,
+    price: 20,
+    img: 'images/buffalo/BuffaloSpicyPizza.jpg',
+    quantity: 1
+},
+{
+    id: 9,
+    name: 'Friends Pizza',
+    category : 'biryani',
+    rating : 4.3,
+    price: 15,
+    img: 'images/buffalo/bigChickenPizza.jpg',
+    quantity: 1
+},
+{
+    id: 10,
+    name: 'Classic Hawaiian Pizza',
+    category : 'chicken',
+    rating : 4.3,
+    price: 11,
+    img: 'images/hawaiian/classicHawaiianPizza.jpg',
+    quantity: 1
+},
+{
+    id: 11,
+    name: 'Hawaian Cheese Pizza',
+    category : 'chicken',
+    rating : 4.3,
+    price: 10,
+    img: 'images/hawaiian/hawaiianCheesePizza.jpg',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'Meaty Hawaiian Pizza',
+    category : 'chicken',
+    rating : 4.3,
+    price: 14,
+    img: 'images/hawaiian/hawaiianMeatyPizza.jpg',
+    quantity: 1
+},
+{
+    id: 13,
+    name: 'Pasta Bake Pizza',
+    category : 'chicken',
+    rating : 4.3,
+    price: 12,
+    img: 'images/hawaiian/hawaiianPastaBakePizza.jpg',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'Hawaiian Pizza',
+    category : 'chicken',
+    rating : 4.3,
+    price: 17,
+    img: 'images/hawaiian/hawaiianPizza.jpg',
+    quantity: 1
+},
+{
+    id: 15,
+    name: 'Hawaiian Spicy Pizza',
+    category : 'chicken',
+    rating : 4.3,
+    price: 20,
+    img: 'images/hawaiian/hawaiianSpicyPizza.jpg',
+    quantity: 1
+},
+{
+    id: 16,
+    name: 'Classic Pizza.jpg',
+    category : 'paneer',
+    rating : 4.3,
+    price: 15,
+    img: 'images/margherita/classicMargheritaPizza.jpg',
+    quantity: 1
+
+},
+{
+    id: 17,
+    name: 'Cheese Pizza',
+    category : 'paneer',
+    rating : 4.3,
+    price: 10,
+    img: 'images/margherita/MargheritaCheesePizza.jpg',
+    quantity: 1
+},
+{
+    id: 18,
+    name: 'Pesto Ricotto Pizza',
+    category : 'paneer',
+    rating : 4.3,
+    price: 15,
+    img: 'images/margherita/margheritaPizzaWithPestoRicotto.jpg',
+    quantity: 1
+
+},
+{
+    id: 19,
+    name: 'Mega Pizza',
+    category : 'paneer',
+    rating : 4.3,
+    price: 12,
+    img: 'images/margherita/megaMargheritaPizza.jpg',
+    quantity: 1
+
+},
+{
+    id: 20,
+    name: 'Spicy Pizza',
+    category : 'paneer',
+    rating : 4.3,
+    price: 8,
+    img: 'images/margherita/spicyMargheritaPizza.jpg',
+    quantity: 1
+},
+{
+    id: 21,
+    name: 'Tomato Pizza',
+    category : 'paneer',
+    rating : 4.3,
+    price: 7,
+    img: 'images/margherita/tomato,basilAndMozzarellaPizza.jpg',
+    quantity: 1
+},
+{
+    id: 22,
+    name: 'IDU Pizza',
+    age: '32',
+    category : 'paneer',
+    rating : 4.3,
+    price: 5,
+    img: 'images/margherita/MargheritaCheesePizza.jpg',
+    quantity: 1
+},
+{
+    id: 23,
+    name: 'Horse Meat Pizza',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 8,
+    img: 'images/meat/horseMeatPizza.jpg',
+    quantity: 1
+},
+{
+    id: 24,
+    name: 'Lamb Pizza',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 7,
+    img: 'images/meat/lambMeatPizza.jpg',
+    quantity: 1
+},
+{
+    id: 25,
+    name: 'Lovers Pizza',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 5,
+    img: 'images/meat/meatyPizza.jpg',
+    quantity: 1
+},
+{
+    id: 26,
+    name: 'Meaty Pizza',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 7,
+    img: 'images/meat/meatyPizza.jpg',
+    quantity: 1
+},
+{
+    id: 27,
+    name: 'Mega meat Pizza',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 10,
+    img: 'images/meat/megaMeatLoversPizza.jpg',
+    quantity: 1
+},
+{
+    id: 28,
+    name: 'Sausage Pizza',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 4,
+    img: 'images/meat/super-sausagePizza.jpg',
+    quantity: 1
+},
+{
+    id: 29,
+    name: 'Free Pizza',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 4,
+    img: 'images/meat/lambMeatPizza.jpg',
+    quantity: 1
+},
+{
+    id: 30,
+    name: 'Big Pepperoni P..',
+    category : 'chinese',
+    rating : 4.3,
+    price: 8,
+    img: 'images/pepperoni/bigPepperoniPizza.jpg',
+    quantity: 1
+},
+{
+    id: 31,
+    name: 'Italian Pizza',
+    category : 'chinese',
+    rating : 4.3,
+    price: 7,
+    img: 'images/pepperoni/italianPizza.jpg',
+    quantity: 1
+},
+{
+    id: 32,
+    name: 'Cheese Pizza',
+    category : 'chinese',
+    rating : 4.3,
+    price: 5,
+    img: 'images/pepperoni/pepperoniCheesePizza.jpg',
+    quantity: 1
+},
+{
+    id: 33,
+    name: 'Crispy Pizza',
+    category : 'chinese',
+    rating : 4.3,
+    price: 16,
+    img: 'images/pepperoni/PepperoniCrispyPizza.jpeg',
+    quantity: 1
+},
+{
+    id: 34,
+    name: 'Pepperoni Pizza',
+    category : 'chinese',
+    rating : 4.3,
+    price: 14,
+    img: 'images/pepperoni/pepperoniPizza.jpg',
+    quantity: 1
+},
+{
+    id: 35,
+    name: 'Spicy Pepperoni',
+    category : 'chinese',
+    rating : 4.3,
+    price: 10,
+    img: 'images/pepperoni/PepperoniCrispyPizza.jpeg',
+    quantity: 1
+},
+{
+    id: 36,
+    name: 'Classic Pizza',
+    category : 'chinese',
+    rating : 4.3,
+    price: 8,
+    img: 'images/pepperoni/pepperoniCheesePizza.jpg',
+    quantity: 1
+},
+{
+    id: 37,
+    name: 'LOL Pizza',
+    category : 'south indian',
+    rating : 4.3,
+    price: 18,
+    img: 'images/veggie/ClassicPizza.jpg',
+    quantity: 1
+},
+{
+    id: 38,
+    name: 'Cucumber Pizza',
+    category : 'south indian',
+    rating : 4.3,
+    price: 20,
+    img: 'images/veggie/cucumberPizza.jpg',
+    quantity: 1
+},
+{
+    id: 39,
+    name: 'Grilled Pizza',
+    category : 'south indian',
+    rating : 4.3,
+    price: 12,
+    img: 'images/veggie/grilled pizza.jpg',
+    quantity: 1
+},
+{
+    id: 40,
+    name: 'Spicy Pizza',
+    category : 'south indian',
+    rating : 4.3,
+    price: 10,
+    img: 'images/veggie/SpicyPizza.jpg',
+    quantity: 1
+},
+{
+    id: 41,
+    name: 'Pizza',
+    category : 'south indian',
+    rating : 4.3,
+    price: 15,
+    img: 'images/veggie/grilled pizza.jpg',
+    quantity: 1
+},
+{
+    id: 42,
+    name: 'Supreme Pizza',
+    category : 'south indian',
+    rating : 4.3,
+    price: 40,
+    img: 'images/veggie/SupremePizza.png',
+    quantity: 1
+},
+{
+    id: 43,
+    name: 'Veg Pizza',
+    category : 'south indian',
+    rating : 4.3,
+    price: 25,
+    img: 'images/veggie/cucumberPizza.jpg',
+    quantity: 1
+},
+{
+    id: 44,
+    name: 'Sous Pizza',
+    category : 'south indian',
+    rating : 4.3,
+    price: 34,
+    img: 'images/veggie/SpicyPizza.jpg',
+    quantity: 1
+},
+]
+
+export {foodItem};
